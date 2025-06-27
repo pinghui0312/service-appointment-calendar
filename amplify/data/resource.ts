e87fd50a28e09cd484280056479c96014633e171
khoph <pinghui_0312@hotmail.com>
2025-06-27 17:00:30 +0800

    feat: add appointment table

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,15 +1,17 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 /*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
+The section below creates a appointment database table with a "content" field. Try
 adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
+"update", and "delete" any "appointment" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  Appointment: a
     .model({
-      content: a.string(),
+      dentistName: a.string(),
+      equipment: a.string(),
+      notes: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
@@ -20,7 +22,6 @@ export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
@@ -52,6 +53,6 @@ Fetch records from the database and use them in your frontend component.
 
 /* For example, in a React component, you can use this snippet in your
   function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
+// const { data: appointments } = await client.models.appointment.list()
 
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
+// return <ul>{appointments.map(appointment => <li key={appointment.id}>{appointment.content}</li>)}</ul>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,53 +1,54 @@
 <script setup lang="ts">
-import '@/assets/main.css';
-import { onMounted, ref } from 'vue';
-import type { Schema } from '../../amplify/data/resource';
-import { generateClient } from 'aws-amplify/data';
+import "@/assets/main.css";
+import { onMounted, ref } from "vue";
+import type { Schema } from "../../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
-// create a reactive reference to the array of todos
-const todos = ref<Array<Schema['Todo']["type"]>>([]);
+// create a reactive reference to the array of appointments
+const appointments = ref<Array<Schema["Appointment"]["type"]>>([]);
 
-function listTodos() {
-  client.models.Todo.observeQuery().subscribe({
+const listAppointments = () => {
+  client.models.Appointment.observeQuery().subscribe({
     next: ({ items, isSynced }) => {
-      todos.value = items
-     },
-  }); 
-}
-
-function createTodo() {
-  client.models.Todo.create({
-    content: window.prompt("Todo content")
-  }).then(() => {
-    // After creating a new todo, update the list of todos
-    listTodos();
+      appointments.value = items;
+    },
   });
-}
-    
-// fetch todos when the component is mounted
- onMounted(() => {
-  listTodos();
-});
+};
 
+const createAppointment = () => {
+  client.models.Appointment.create({
+    dentistName: "testing",
+    equipment: "testing",
+    notes: "testing",
+  }).then(() => {
+    // After creating a new appointment, update the list of appointments
+    listAppointments();
+  });
+};
+
+// fetch appointments when the component is mounted
+onMounted(() => {
+  listAppointments();
+});
 </script>
 
 <template>
   <main>
-    <h1>My todos</h1>
-    <button @click="createTodo">+ new</button>
+    <h1>My appointments</h1>
+    <button @click="createAppointment">+ new</button>
     <ul>
-      <li 
-        v-for="todo in todos" 
-        :key="todo.id">
-        {{ todo.content }}
+      <li v-for="appointment in appointments" :key="appointment.id">
+        {{ appointment.notes }}
       </li>
     </ul>
     <div>
-      🥳 App successfully hosted. Try creating a new todo.
+      🥳 App successfully hosted. Try creating a new appointment.
       <br />
-      <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
+      <a
+        href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/"
+      >
         Review next steps of this tutorial.
       </a>
     </div>
