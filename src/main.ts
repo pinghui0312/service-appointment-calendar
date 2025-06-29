7fabb71b3a0ec53e0346cab2b17a98ece0fe024b
khoph0312 <pinghui_0312@hotmail.com>
2025-06-29 22:14:26 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import "./styles.css";
 
 Amplify.configure(outputs);
 
